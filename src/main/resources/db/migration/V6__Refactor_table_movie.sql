b86c08c9082fa3b0b76e9228c0ba707cf894ead9
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-24 18:13:53 -0300

    refactor: remove foreign key constraints and columns for category and streaming in tb_movie table

---
+++ src/main/resources/db/migration/V6__Refactor_table_movie.sql
@@ -0,0 +1,4 @@
+ALTER TABLE tb_movie DROP CONSTRAINT fk_movie_category;
+ALTER TABLE tb movie DROP CONSTRAINT fk_movie_streaming;
+ALTER TABLE tb_movie DROP COLUMN category_id;
+ALTER TABLE tb_movie DROP COLUMN streaming_id;
